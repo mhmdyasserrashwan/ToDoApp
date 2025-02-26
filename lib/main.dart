0ede22f39d9b1842dff1b65314a0956145c78148
mhmdyasserrashwan <mhmdyasserrashwan@gmail.com>
2025-02-26 19:52:47 +0200

    images added

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:todoapp/app/app.dart';
 import 'package:todoapp/core/utilis/app_strings.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: AppStringsMYR.appName,
-    home: Myapp(),
-  ));
+  runApp(
+    Myapp(),
+  );
 }
