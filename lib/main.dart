7302c2193c80c4dc0e9183fabe0987e70721a91b
mhmdyasserrashwan <mhmdyasserrashwan@gmail.com>
2025-02-26 11:51:49 +0200

    added new colors strings and assetsfolders

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/app/app.dart';
+import 'package:todoapp/core/utilis/app_strings.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SplashScreen(),
+    debugShowCheckedModeBanner: false,
+    title: AppStringsMYR.appName,
+    home: Myapp(),
   ));
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //appBar: AppBar(title: Text(data),),
-      body: Column(
-        children: [],
-      ),
-    );
-  }
 }
